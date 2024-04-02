339901bfd853ac83a1310a075bb58c7c798a606e
fridayaj <fridayaj@mail.uc.edu>
2024-04-02 11:26:30 -0400

    Add a constructor to the new class named after my 6+2

---
+++ PrimeNumberFun/fridayaj.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberFun
+{
+    internal class fridayaj
+    {
+        // Add a constructor that prints "Hello"
+
+        public fridayaj()
+        {
+            Console.WriteLine("Hello");
+        }
+
+    }
+}
